94a3cd7f78161afdae9155af36c25f3c5aaef264
Cody Heiser <codyheiser49@gmail.com>
2020-09-21 14:34:25 -0500

    gridspec err in qc_summary

--- dropkick/qc.py
+++ dropkick/qc.py
@@ -143,6 +143,7 @@ def counts_plot(adata, show=False, genes=True, ambient=True, mito=True, ax=None)
                     np.argsort(adata.obs.total_counts)[::-1]
                 ].values,
                 s=18,
+                color="b",
                 alpha=0.3,
                 edgecolors="none",
                 label="% Ambient",
@@ -154,6 +155,7 @@ def counts_plot(adata, show=False, genes=True, ambient=True, mito=True, ax=None)
                     np.argsort(adata.obs.total_counts)[::-1]
                 ].values,
                 s=18,
+                color="r",
                 alpha=0.3,
                 edgecolors="none",
                 label="% Mito",
@@ -187,8 +189,8 @@ def qc_summary(adata, mito=True, fig=None, save_to=None, verbose=True):
         fig = plt.figure(figsize=(5, 10))
     # arrange axes as subplots
     gs = gridspec.GridSpec(1, 3, figure=fig)
-    ax1 = plt.subplot(gs[0, 0:3])
-    ax2 = plt.subplot(gs[0, 3])
+    ax1 = plt.subplot(gs[0:3])
+    ax2 = plt.subplot(gs[3])
     # add plots to axes
     counts_plot(adata, ax=ax1, show=False, mito=mito)
     dropout_plot(adata, ax=ax2, show=False)
